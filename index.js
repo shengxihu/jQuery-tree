2d969dc2e60a32dbbd0a398e3df066e0a62a2906
shengxihu <1679112936@qq.com>
2016-02-28 16:15:39 +0800

    使用addClass添加样式

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 $(function(){
-	$("ul").css({
-		"list-style-image": "url(images/item_files.svg)",
-		"list-style-type":"square"
-	});
-	$("li:has('ul')").css({
-		"list-style-image": "url(images/close_files.svg)",
-		"list-style-type":"square"
-	})
+	$("li:has('ul')").addClass('files')
 		.bind('click',function(event){
-			if ($(this).children().is(":visible")) {
-				$(this).children().hide('fast', function() {
-					$(this).parent().css({
-						"list-style-image": "url(images/close_files.svg)",
-						"list-style-type":"square"
-					})
-				});
-			}else{
-				$(this).children().show('fast', function() {
-					$(this).parent().css({
-						"list-style-image": "url(images/open_files.svg)",
-						"list-style-type":"square"
-					})
-				});
+			if(this==event.target){
+				if ($(this).children().is(":visible")) {
+					$(this).children().slideUp('fast', function() {
+						$(this).parent().removeClass()
+							.addClass('close_files')
+					});
+				}else{
+					$(this).children().slideDown('fast', function() {
+						$(this).parent().removeClass()
+							.addClass('open_files')
+					});
+				}
 			}
 			event.stopPropagation();
 		})
-		.children().css({
-			"display":"none"
-		})
+		.children().addClass('inner_ul')
 })
